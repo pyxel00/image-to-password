8fc4de5072bc94f400a4a83f829a83809f5d582d
pyxel00 <pyxel143@gmail.com>
2024-02-27 20:40:54 +0300

    updated repo

---
+++ README.md
@@ -0,0 +1,16 @@
+# A small command line utility for turning images to string.
+## for usage in passwords and other stuff.
+### Written in python
+
+### How to use:
+
+Run the password.py with
+
+```python3 /path-to-file/password.py```
+
+It will ask you for a path to the image you want to convert to string.
+After entering the path and running the program output will be stored inside the string.txt file.
+
+String will be broken into 35 character long parts to not exceed the character limit.
+
+I wouldn't recommend using big images (like 1920x1080 or something like that) since big files can cause bigger txt files and longer strings. But if you want to go for it.
---
+++ password.py
@@ -0,0 +1,39 @@
+import base64
+
+def createPassword():
+    # User input for the file
+    path = input("Enter the path of the file: ")
+
+    # Reading file
+    openedFile = open(path, 'rb')
+    image = openedFile.read()
+
+    # Encoding the file
+    encodedImage = base64.a85encode(image)
+
+    # Make the text impossible to reverse engineer without knowing which tool was used
+    utf8Name = path.encode('utf-8')
+    encodedName = base64.b85encode(utf8Name)
+    addedImage = str(encodedName + encodedImage + encodedName)
+
+    # Format the string
+    formattedImage = stringFormat(addedImage, 35)
+
+    # Crate and store formattedImage
+    textFile = open('string.txt', 'w')
+    textFile.write(str(formattedImage))
+
+def stringFormat(text, charLimit):
+    newText = ''
+    currentText = ''
+
+    for char in text:
+        currentText += str(char)
+        if len(currentText) == charLimit:
+            newText += currentText + '\n'
+            currentText = ''
+            
+    newText += currentText
+    return newText
+
+createPassword()
